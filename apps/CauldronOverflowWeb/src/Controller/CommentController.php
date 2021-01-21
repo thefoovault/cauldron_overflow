110fbff403d651b71ba9b056e7d9314808355684
Thefoovault <marta.ag.ma@gmail.com>
2021-01-21 16:18:05 +0100

    Added logger when voting

--- apps/CauldronOverflowWeb/src/Controller/CommentController.php
+++ apps/CauldronOverflowWeb/src/Controller/CommentController.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace CauldronOverflowWeb\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class CommentController extends AbstractController
 {
-    public function commentVote($id, $direction): Response
+    public function commentVote($id, $direction, LoggerInterface $logger): Response
     {
         if ($direction === 'up') {
+            $logger->info('Voted up');
             $currentVoteCount = rand(7,100);
         } else {
+            $logger->info('Voted down');
             $currentVoteCount = rand(0,5);
         }
 
