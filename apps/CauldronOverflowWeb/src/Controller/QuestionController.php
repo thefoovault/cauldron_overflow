35c74cb4aa9cc6d526ea91420239a3a3134d9fb5
Thefoovault <marta.ag.ma@gmail.com>
2021-01-24 12:14:07 +0100

    Added markdown parser

--- apps/CauldronOverflowWeb/src/Controller/QuestionController.php
+++ apps/CauldronOverflowWeb/src/Controller/QuestionController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CauldronOverflowWeb\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,17 +15,21 @@ class QuestionController extends AbstractController
         return $this->render('question/homepage.html.twig');
     }
 
-    public function show($slug): Response
+    public function show($slug, MarkdownParserInterface $markdownParser): Response
     {
         $answers = [
-            "First answer",
-            "Second answer",
-            "Third answer"
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
         ];
+
+        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+
         return $this->render('question/show.html.twig',
             [
                 "question" => ucwords(str_replace('-', ' ', $slug)),
-                "answers" => $answers
+                "answers" => $answers,
+                'questionText' => $markdownParser->transformMarkdown($questionText),
             ]
         );
     }
