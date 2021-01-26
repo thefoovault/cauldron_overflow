f7eb5e273752cbaad5933391e95d822eb64aef3a
Thefoovault <marta.ag.ma@gmail.com>
2021-01-26 17:31:52 +0100

    Added a not autowirable argument into MarkdownHelper

--- apps/CauldronOverflowWeb/src/Controller/QuestionController.php
+++ apps/CauldronOverflowWeb/src/Controller/QuestionController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace CauldronOverflowWeb\Controller;
 
-use CauldronOverflowWeb\Services\MarkdownHelper;
+use CauldronOverflow\Services\MarkdownHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
--- src/CauldronOverflow/Services/MarkdownHelper.php
+++ src/CauldronOverflow/Services/MarkdownHelper.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace CauldronOverflowWeb\Services;
+namespace CauldronOverflow\Services;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Contracts\Cache\CacheInterface;
@@ -11,15 +11,21 @@ class MarkdownHelper
 {
     private CacheInterface $cache;
     private MarkdownParserInterface $markdownParser;
+    private bool $isDebug;
 
-    public function __construct(CacheInterface $cache, MarkdownParserInterface $markdownParser)
+    public function __construct(CacheInterface $cache, MarkdownParserInterface $markdownParser, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdownParser = $markdownParser;
+        $this->isDebug = $isDebug;
     }
 
     public function parse(string $source): string
     {
+        if ($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
+
         return $this->cache->get('markdown_'.md5($source), function() use ($source) {
             return $this->markdownParser->transformMarkdown($source);
         });
