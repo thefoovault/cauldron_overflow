cd17a7bdf1bead150f2172fe47d40ea5f7754665
Thefoovault <marta.ag.ma@gmail.com>
2021-01-30 12:00:45 +0100

    Added maker package and tested with new symfony command

--- apps/CauldronOverflowWeb/config/bundles.php
+++ apps/CauldronOverflowWeb/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
---
+++ apps/CauldronOverflowWeb/src/Command/RandomSpellCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CauldronOverflowWeb\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class RandomSpellCommand extends Command
+{
+    protected static $defaultName = 'app:random-spell';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Cast a random spell')
+            ->addArgument('your-name', InputArgument::OPTIONAL, 'Your name')
+            ->addOption('yell', null, InputOption::VALUE_NONE, 'Yell?')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $yourName = $input->getArgument('your-name');
+
+        if ($yourName) {
+            $io->note(sprintf('Hi %s!', $yourName));
+        }
+
+        $spells = [
+            'alohomora',
+            'confundo',
+            'engorgio',
+            'expecto patronum',
+            'expelliarmus',
+            'impedimenta',
+            'reparo',
+        ];
+
+        $spell = $spells[array_rand($spells)];
+
+        if ($input->getOption('yell')) {
+            $spell = strtoupper($spell);
+        }
+
+        $io->success($spell);
+
+        return Command::SUCCESS;
+    }
+}
