902b18a4e3ced0b52101cef3028584507fa1e4f0
Thefoovault <marta.ag.ma@gmail.com>
2021-02-05 17:32:33 +0100

    Modified question to hav a unique constraint for slug

---
+++ apps/CauldronOverflowWeb/migrations/Version20210204191433.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210204191433 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX unique_slug ON question (slug)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX unique_slug ON question');
+    }
+}
