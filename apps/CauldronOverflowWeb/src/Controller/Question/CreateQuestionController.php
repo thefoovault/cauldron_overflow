6ec172684997c4b1e5981af8ce05a18cbbcea20a
Thefoovault <marta.ag.ma@gmail.com>
2021-02-16 15:50:00 +0100

    Remove unneeded dependencies

--- apps/CauldronOverflowWeb/src/Controller/Question/CreateQuestionController.php
+++ apps/CauldronOverflowWeb/src/Controller/Question/CreateQuestionController.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace CauldronOverflowWeb\Controller\Question;
 
 use CauldronOverflow\Application\Question\Create\CreateQuestionCommand;
-use CauldronOverflow\Domain\Question\QuestionRepository;
 use Shared\Infrastructure\Symfony\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class CreateQuestionController extends Controller
 {
-    public function __invoke(Request $request, QuestionRepository $questionRepository): Response
+    public function __invoke(Request $request): Response
     {
         $parameters = json_decode($request->getContent(), true);
 
